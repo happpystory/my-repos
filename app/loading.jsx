92253589a189005c18059bf9e5547228cb7469f3
Tisho <tishoyanchev@yahoo.com>
2024-09-28 15:56:56 +0200

    updated config file

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LoadingPage = () => {
   return (
     <div className='loader'>
-        <div className="spinner"></div>
+      <div className="spinner"></div>
     </div>
   )
 }
